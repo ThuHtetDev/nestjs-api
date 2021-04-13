2be1a43e21b0271cd450eebc2897f1c1516342a1
ThuHtetDev <thuhtettun.dev@gmail.com>
2021-04-13 21:30:56 +0630

    controller req and resp

---
+++ src/items/dto/create-item.dto.ts
@@ -0,0 +1,6 @@
+export class CreateItemDto{
+    name: string;
+    description: string;
+    qty: number;
+    price: number;
+}
--- src/items/items.controller.ts
+++ src/items/items.controller.ts
@@ -1,4 +1,31 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, HttpCode, Param, Post, Put } from '@nestjs/common';
+import { CreateItemDto } from './dto/create-item.dto';
 
 @Controller('items')
-export class ItemsController {}
+export class ItemsController {
+    @Get()
+    findAll():string {
+        return 'get all items';
+    }
+
+    @Post()
+    @HttpCode(201)
+    create(@Body() createItemDto:CreateItemDto):string {
+        return `Name: ${createItemDto.name} Price: ${createItemDto.price}`;
+    }
+
+    @Get(':item')
+    findOne(@Param() param):string {
+        return `Item: ${param.item}`;
+    }
+
+    @Delete(':item')
+    delete(@Param('item') item):string {
+        return `Deleted ${item}`;
+    }
+
+    @Put(':item')
+    update(@Param('item') item, @Body() updateItemDto: CreateItemDto):string {
+        return `${item} -- ${updateItemDto.name}`
+    }
+}
